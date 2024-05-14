9ea5d4e5e5d6b8c9326c6e27c33b81f1f581da02
ZARRAR-1 <mack4009@gmail.com>
2024-05-14 22:06:49 +0500

    Remaining Tasks: 1) Splash Screen 2) Welcome Sound on splash

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  return runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  final player = AudioPlayer();
+
+  XylophoneApp({super.key});
+
+  Future<void> playSound({required int soundNumber}) async {
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({required soundNum, required bgColor}) {
+    return Expanded(
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: bgColor),
+          onPressed: () async {
+            playSound(soundNumber: soundNum);
+          },
+          child: const Text('')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // showSemanticsDebugger: true, preventing sound
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Center(child: Text('My Xylophone',style: TextStyle(backgroundColor: Colors.black, fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25))),
+                buildKey(soundNum: 1, bgColor: Colors.red),
+                buildKey(soundNum: 2, bgColor: Colors.orange),
+                buildKey(soundNum: 3, bgColor: Colors.yellow),
+                buildKey(soundNum: 4, bgColor: Colors.green),
+                buildKey(soundNum: 5, bgColor: Colors.teal),
+                buildKey(soundNum: 6, bgColor: Colors.lightBlue),
+                buildKey(soundNum: 7, bgColor: Colors.purpleAccent),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
