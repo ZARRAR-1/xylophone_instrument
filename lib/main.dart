66a7873fead1189f90fe9d067152765fbde2c624
ZARRAR-1 <mack4009@gmail.com>
2024-10-24 10:21:29 +0500

    Formatted the layout of Xylophone. Need to make a custom Elevated Button with holes on both ends.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,8 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Future.delayed(const Duration(seconds: 5));
+  await Future.delayed(const Duration(seconds:2));
   FlutterNativeSplash.remove();
-
 
   return runApp(XylophoneApp());
 }
@@ -22,6 +21,7 @@ class XylophoneApp extends StatelessWidget {
 
   Expanded buildKey({required soundNum, required bgColor}) {
     return Expanded(
+      flex: 5,
       child: ElevatedButton(
           style: ElevatedButton.styleFrom(backgroundColor: bgColor),
           onPressed: () async {
@@ -34,23 +34,87 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // showSemanticsDebugger: true, preventing sound
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const Center(child: Text('My Xylophone',style: TextStyle(backgroundColor: Colors.black, fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25))),
-                buildKey(soundNum: 1, bgColor: Colors.red),
-                buildKey(soundNum: 2, bgColor: Colors.orange),
-                buildKey(soundNum: 3, bgColor: Colors.yellow),
-                buildKey(soundNum: 4, bgColor: Colors.green),
-                buildKey(soundNum: 5, bgColor: Colors.teal),
-                buildKey(soundNum: 6, bgColor: Colors.lightBlue),
-                buildKey(soundNum: 7, bgColor: Colors.purpleAccent),
+                const Center(
+                  child: Text(
+                    'My Xylophone',
+                    style: TextStyle(
+                      backgroundColor: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 1.0, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 0.5), // Add padding
+                    child: buildKey(soundNum: 7, bgColor: Colors.purpleAccent),//purpleAccent
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.95, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5,2, 5,0.5), // Add padding
+                    child: buildKey(soundNum: 6, bgColor: Colors.lightBlue),//lightBlue
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.85, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5,2, 5,0.5), // Add padding
+                    child: buildKey(soundNum: 5, bgColor: Colors.teal),//teal
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.75, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding:const EdgeInsets.fromLTRB(5,2, 5,0.5), // Add padding
+                    child: buildKey(soundNum: 4, bgColor: Colors.green),
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.65, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5,2, 5,0.5), // Add padding
+                    child: buildKey(soundNum:3, bgColor: Colors.yellow),//yellow
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.55, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5,2, 5,0.5), // Add padding
+                    child: buildKey(soundNum: 2, bgColor: Colors.orange),//orange
+                  ),
+                ),
+                SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.45, // Adjust width
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 5), // Add padding
+                    child: buildKey(soundNum: 1, bgColor: Colors.red),//red
+                  ),
+                ),
               ],
             ),
           ),
