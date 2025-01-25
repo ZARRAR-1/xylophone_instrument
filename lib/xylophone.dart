8185eea3d90696e3c8e1dadde78f180bbc6f4464
ZARRAR-1 <mack4009@gmail.com>
2025-01-25 16:49:52 +0500

    Added linter rules   prefer_single_quotes: true   discarded_futures: true   unawaited_futures: true Added logic to take username as input or show default name. However, AlertDialog not appearing. Native splash plugin removed. Still Working on Splash Screen.
    Remaining Tasks:
    1)Welcome Sound on splash
    2) AlertDialog

--- lib/xylophone.dart
+++ lib/xylophone.dart
@@ -3,7 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class XylophoneApp extends StatefulWidget {
-  XylophoneApp({super.key});
+  const XylophoneApp({super.key});
 
   @override
   State<XylophoneApp> createState() => _XylophoneAppState();
